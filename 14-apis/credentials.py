c589306fdcf3b05d4365d067e6ba37efe491beb3
Kate Isaacs <kisaacs@acm.org>
2024-02-27 14:05:42 -0700

    Adding API key part.

---
+++ 14-apis/credentials.py
@@ -0,0 +1,8 @@
+# Fill these in with your keys. You may not need all of them.
+API_KEY = ""
+API_KEY_SECRET = ""
+BEARER_TOKEN = ""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+CLIENT_ID = ""
+CLIENT_SECRET = ""
